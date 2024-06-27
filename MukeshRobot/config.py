16466240476271aad727ab93cd61fec4c6027a00
82kpro_Official <161997419+UNNI12345YT@users.noreply.github.com>
2024-06-27 13:12:12 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -4,19 +4,19 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "" # integer value, dont use ""
-    API_HASH = ""
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    API_ID = 27408015 # integer value, dont use ""
+    API_HASH = "2f07e7c921c8d2b982df12d65a46ca46"
+    TOKEN = "7474172596:AAFKvnrFM5a25xCOzVgejImLRTujDEsQ3ec"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 6171681404 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "MRXSUPPORT"  # Your own group for support, do not add the @
     START_IMG = ""
     EVENT_LOGS = (-1002090641762)  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= ""
+    MONGO_DB_URI= "mongodb+srv://Achu:Achu@cluster0.shz28r2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # RECOMMENDED
     DATABASE_URL = ""  # A sql database url from elephantsql.com
     CASH_API_KEY = (
-        ""  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "WYHK8S2IRO0M50BX"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
     TIME_API_KEY = ""
     
@@ -25,11 +25,11 @@ class Config(object):
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
-    DEMONS = []  # User id of support users
-    TIGERS = []  # User id of tiger users
-    WOLVES = []  # User id of whitelist users
+    DRAGONS = [6171681404]  # User id of sudo users
+    DEV_USERS = [6171681404]  # User id of dev users
+    DEMONS = [5758713974]  # User id of support users
+    TIGERS = [5758713974]  # User id of tiger users
+    WOLVES = [6171681404]  # User id of whitelist users
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
